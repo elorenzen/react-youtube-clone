c17f2d78abbb58f18c728200d88b8fe59dc6c84d
elorenzen <eric.lorenzen@gmail.com>
2019-05-27 13:36:13 -0600

    Initiated callback in onFormSubmit function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,6 +11,7 @@ class SearchBar extends React.Component{
         event.preventDefault();
 
         // Initiate callback from parent component
+        this.props.onTermSubmit(this.state.term);
     }
 
     render() {
