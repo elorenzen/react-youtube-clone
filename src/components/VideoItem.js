ce1311d8811eaa29d0bdfa85e874c3df5d1f5050
elorenzen <eric.lorenzen@gmail.com>
2019-06-01 09:22:21 -0600

    Removed div containing video description

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -10,7 +10,6 @@ const VideoItem = ({video, onVideoSelect}) => {
                 <a className="header" href={watchLink}>
                     {video.snippet.title}
                 </a>
-                <div className="description">{video.snippet.description}</div>
             </div>
         </div>
     )
