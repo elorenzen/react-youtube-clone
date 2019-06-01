fb01e75ea9909849bddb703c89b99a4d0614e0e8
elorenzen <eric.lorenzen@gmail.com>
2019-06-01 09:38:45 -0600

    Changed alt to snippet.title

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,7 +5,7 @@ const VideoItem = ({video, onVideoSelect}) => {
 
     return (
         <div className="item" onClick={() => onVideoSelect(video)}>
-            <img className="ui image" src={video.snippet.thumbnails.default.url} alt={video.snippet.description} />
+            <img className="ui image" src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
             <div className="content">
                 <a className="header" href={watchLink}>
                     {video.snippet.title}
