7934fd17cb37190cad0476e8dbdc680319748801
elorenzen <eric.lorenzen@gmail.com>
2019-06-01 07:51:08 -0600

    Passed onVideoSelect into JSX as onClick event handler with video parameter passed in

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const VideoItem = ({video}) => {
+const VideoItem = ({video, onVideoSelect}) => {
     const watchLink=`https://www.youtube.com/watch?v=${video.id.videoId}`;
 
     return (
-        <div className="item">
+        <div className="item" onClick={() => onVideoSelect(video)}>
             <img className="ui image" src={video.snippet.thumbnails.default.url} alt={video.snippet.description} />
             <div className="content">
                 <a className="header" href={watchLink}>
