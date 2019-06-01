f650a5387d36c44684370c5278a486fb13997d55
elorenzen <eric.lorenzen@gmail.com>
2019-06-01 09:08:28 -0600

    Imported VideoDetail component and passed video attribute with selectedVideo value

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 import youtube from '../apis/youtube';
+import VideoDetail from '../components/VideoDetail';
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
@@ -17,13 +18,14 @@ class App extends React.Component {
 
     // video = fetched video object from YouTube API
     onVideoSelect = (video) => {
-        console.log('From the App', video);
+        this.setState({ selectedVideo: video });
     }
 
     render() {
         return (
         <div className='ui container'>
             <SearchBar onTermSubmit={this.onTermSubmit} />
+            <VideoDetail video={this.state.selectedVideo}></VideoDetail>
             <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
         </div>
         )
